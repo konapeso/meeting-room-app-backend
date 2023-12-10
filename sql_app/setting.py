0f4567163c2c39269da1b0c8c9e2ffb585ff3bf9
konapeso <n2kgt1@gmail.com>
2023-12-10 20:04:13 +0900

    RDSの設定に変更

--- sql_app/setting.py
+++ sql_app/setting.py
@@ -2,10 +2,11 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker, declarative_base
 from pydantic import BaseModel
 
+username = "root"
+password = "zme0aju_hpe1ebm*BWY"
+host = "eve-backend-db.cwoji7dwklcq.ap-southeast-2.rds.amazonaws.com"
 # データベースエンジンの設定
-engine = create_engine(
-    "mysql+pymysql://user:userpassword@localhost:3306/demo", echo=True
-)
+engine = create_engine(f"mysql+pymysql://{username}:{password}@{host}", echo=True)
 
 # ベースクラスの作成
 Base = declarative_base()
