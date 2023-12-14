a5b57b65afe32852d2b1b861612d21d9164404b0
konapeso <n2kgt1@gmail.com>
2023-12-14 20:48:52 +0900

    文字数制限を追加

--- sql_app/models.py
+++ sql_app/models.py
@@ -6,14 +6,14 @@ class User(Base):
     __tablename__ = "users"
 
     user_id = Column(Integer, primary_key=True, index=True)
-    user_name = Column(String, unique=True, index=True)
+    user_name = Column(String(255), unique=True, index=True)
 
 
 class Room(Base):
     __tablename__ = "rooms"
 
     room_id = Column(Integer, primary_key=True, index=True)
-    room_name = Column(String, unique=True, index=True)
+    room_name = Column(String(255), unique=True, index=True)
     capacity = Column(Integer)
 
 
