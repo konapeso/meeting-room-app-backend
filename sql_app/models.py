76689de187f4955d637f26cf92b4855eff521d16
konapeso <n2kgt1@gmail.com>
2023-12-14 20:56:40 +0900

    SET NULLの削除

--- sql_app/models.py
+++ sql_app/models.py
@@ -21,12 +21,8 @@ class Booking(Base):
     __tablename__ = "bookings"
 
     booking_id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(
-        Integer, ForeignKey("users.user_id", ondelete="SET NULL"), nullable=False
-    )
-    room_id = Column(
-        Integer, ForeignKey("rooms.room_id", ondelete="SET NULL"), nullable=False
-    )
+    user_id = Column(Integer, ForeignKey("users.user_id"), nullable=False)
+    room_id = Column(Integer, ForeignKey("rooms.room_id"), nullable=False)
     booked_num = Column(Integer)
     start_datetime = Column(DateTime, nullable=False)
     end_datetime = Column(DateTime, nullable=False)
